df259564ea962aca84b975577c1f35684ff1e85d
Burhan Akyuz <burh4n.a@yandex.com>
2022-10-22 11:44:12 +0300

    created dtos

---
+++ RiseContactMicroservice/Rise.Shared/Dtos/CreatePersonWithContactDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Rise.Shared.Dtos;
+
+public class CreatePersonWithContactDto
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Company { get; set; }
+
+    public List<CreateContactDto> Contacts { get; set; }
+}
